fab39fbe828ef3e7314597be0752cfe9606c6c1c
DESKTOP-KEAG5H6\Dima <dimitry.brazhnik@yandex.ru>
2022-10-12 18:13:43 +0300

    added controller

---
+++ core/src/main/java/liga/medical/messageanalyzer/core/service/MessageSenderServiceImpl.java
@@ -0,0 +1,27 @@
+package liga.medical.messageanalyzer.core.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import liga.medical.dto.RabbitMessageDto;
+import liga.medical.messageanalyzer.core.api.MessageSenderService;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MessageSenderServiceImpl implements MessageSenderService {
+
+    private final AmqpTemplate amqpTemplate;
+    private final ObjectMapper objectMapper;
+
+    public MessageSenderServiceImpl(AmqpTemplate amqpTemplate, ObjectMapper objectMapper) {
+        this.amqpTemplate = amqpTemplate;
+        this.objectMapper = objectMapper;
+    }
+
+    @Override
+    public void sendMessage(RabbitMessageDto messageDto, String queue) throws JsonProcessingException {
+        String messageStr = objectMapper.writeValueAsString(messageDto);
+        amqpTemplate.convertAndSend(queue, messageStr);
+        System.out.println(String.format("Сообщение [$s] в очередь [$s] отправлено!", messageStr, queue));
+    }
+}
